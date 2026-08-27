c58115888b9bda1468bbb05d284f7cf36dec6288
robiul-islam <robiul.aub.cse@gmail.com>
2026-08-28 01:52:18 +0600

    Fix dashboard data path

--- dashboard/app.py
+++ dashboard/app.py
@@ -4,7 +4,15 @@ import plotly.express as px
 
 import pandas as pd
 
-df = pd.read_csv("E:/Robiul/Backup_my_own_laptop/Folder_1/Skills/End to End Data project/production-data-analysis/data/processed/production_data_clean.csv")
+
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+DATA_PATH = BASE_DIR / "data" / "processed" / "production_data_clean.csv"
+
+df = pd.read_csv(DATA_PATH)
+
+
 
 st.set_page_config(
     page_title="Production Performance Dashboard",
